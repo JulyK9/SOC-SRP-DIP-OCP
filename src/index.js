524d2a8c23e62c4d8881fb459d997be470408fd2
JKKIM <jkesonjk@gmail.com>
2023-07-11 01:23:54 +0900

    FEAT: authservice를 context API로 작업하기 위한 세팅

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthService } from './service/AuthService';
+import { LocalTokenRepository } from './repository/localTokenRepository';
+import { HttpClient } from './httpClient/httpClient';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +13,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// bootStrap: 처음에 돌아가기 위한 일련의 준비 과정들, 분산되어 있으면 불리함
+// DI
+const tokenRepository = new LocalTokenRepository();
+const httpClient = new HttpClient(
+  'https://www.pre-onboarding-selection-task.shop/',
+  tokenRepository
+);
+const authService = new AuthService(httpClient, tokenRepository);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
