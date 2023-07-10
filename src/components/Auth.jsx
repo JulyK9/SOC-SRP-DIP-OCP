83450ec71dd09b9fc3b5022107b5cbf6ee986ad2
JKKIM <jkesonjk@gmail.com>
2023-07-10 16:59:27 +0900

    feat: Auth 컴포넌트 기본 세팅 작성

---
+++ src/components/Auth.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const Auth = () => {
+  const [userInputs, setUserInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = userInputs;
+
+  const saveUserInputs = ({ target }) => {
+    const { name, value } = target;
+
+    setUserInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const signin = () => {};
+  const signup = () => {};
+  const logout = () => {};
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <div>
+        <label>
+          email
+          <input name="email" value={email} onChange={saveUserInputs} />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input name="password" value={password} onChange={saveUserInputs} />
+        </label>
+      </div>
+      <button onClick={signin}>signin</button>
+      <button onClick={signup}>signup</button>
+      <button onClick={logout}>logout</button>
+    </form>
+  );
+};
+
+export default Auth;
